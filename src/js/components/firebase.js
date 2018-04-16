c94da1b3435ec8f82670bedc6bbf08236306df03
aresobus <aresobus@gmail.com>
2018-04-16 18:58:27 -0400

    firebase changes

---
+++ src/js/components/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+var config = {
+   apiKey: "AIzaSyCA_6MXt-QUw349FkYYEBy7wafkkKRq0Y0",
+   authDomain: "translator-3-1ba6f.firebaseapp.com",
+   databaseURL: "https://translator-3-1ba6f.firebaseio.com",
+   projectId: "translator-3-1ba6f",
+   storageBucket: "",
+   messagingSenderId: "179395182592"
+ };
+ firebase.initializeApp(config);
+
+export default firebase;
